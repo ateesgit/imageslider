60ac171b582908cec29015a27d3966a1e2200279
ateesgit <ateesgit@gmail.com>
2023-09-11 12:19:22 +0530

    first commit

---
+++ lib/screens/imageslider.dart
@@ -0,0 +1,69 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+
+class ImageSliderFirebase extends StatefulWidget {
+  const ImageSliderFirebase({super.key});
+
+  @override
+  State<ImageSliderFirebase> createState() => _ImageSliderFirebaseState();
+}
+
+class _ImageSliderFirebaseState extends State<ImageSliderFirebase> {
+    late Stream<QuerySnapshot> imageStream;
+    int currentSliderIndex = 0 ;
+    CarouselController carouselController =CarouselController();
+
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+  
+    super.initState();
+    var firebase = FirebaseFirestore.instance;
+    imageStream = firebase.collection('donor').snapshots();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+           SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: StreamBuilder<QuerySnapshot>(
+              stream: imageStream,
+              builder:(_,snapshot){
+                if(snapshot.hasData && snapshot.data!.docs.length >1){
+                  return CarouselSlider.builder(
+                    carouselController: carouselController,
+                    itemCount:snapshot.data!.docs.length , 
+                    itemBuilder: (_,index,___){
+                      DocumentSnapshot sliderImage = snapshot.data!.docs[index];
+                      return Image.network(sliderImage['image'],fit: BoxFit.contain);
+                    }, 
+                    options: CarouselOptions(
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      onPageChanged: (index, _) {
+                        setState(() {
+                          currentSliderIndex = index;
+                        });
+                      },
+                    )
+                  );
+                }else{
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              } ),
+           ),
+           const SizedBox(height: 20,),
+           Text("Current Slide Index $currentSliderIndex",style: TextStyle(fontSize: 20),)
+        ],
+      ),
+    );
+  }
+}
